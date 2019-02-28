94286b5a60190a92cd19ef90e6c8ed321a3a6c7e
Stella Rakotonanahary <Stella.Rakotonanahary@keyruscorp.key>
2019-02-28 17:52:52 +0100

    feat(app): almost done missed color drawer. I don't found this className

--- src/components/Dashboard/component.js
+++ src/components/Dashboard/component.js
@@ -4,16 +4,15 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Avatar from "@material-ui/core/Avatar";
 import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Divider from "@material-ui/core/Divider";
 import Drawer from "@material-ui/core/Drawer";
 import FavoriteIcon from "@material-ui/icons/Favorite";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
+import Grid from "@material-ui/core/Grid";
 import Hidden from "@material-ui/core/Hidden";
 import IconButton from "@material-ui/core/IconButton";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
@@ -24,10 +23,18 @@ import ListItemText from "@material-ui/core/ListItemText";
 import MailIcon from "@material-ui/icons/Mail";
 import MenuIcon from "@material-ui/icons/Menu";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SettingsIcon from "@material-ui/icons/Settings";
 import ShareIcon from "@material-ui/icons/Share";
-import tileData from "./tileData";
 import Toolbar from "@material-ui/core/Toolbar";
+import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
+
+import cardDoc from "../../assets/images/card-doc.png";
+import cardGestion from "../../assets/images/card-gestion.png";
+import cardVisio from "../../assets/images/card-visio.png";
+import userOnline1 from "../../assets/images/user1-online.png";
+import userOnline2 from "../../assets/images/user2-online.png";
+import userOnline3 from "../../assets/images/user3-online.png";
 
 export default class Dashboard extends React.Component {
   state = {
@@ -42,22 +49,19 @@ export default class Dashboard extends React.Component {
     const { classes, theme } = this.props;
 
     const drawer = (
-      <div>
-        <div className={classes.toolbar} />
+      <div className={classes.toolbarLeft}>
         <Divider />
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          {["Projects"].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
         </List>
         <Divider />
         <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
+          {["Organizational Behavior", "Management Strategic", "Design Strategy"].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
@@ -73,77 +77,70 @@ export default class Dashboard extends React.Component {
             <IconButton color="inherit" aria-label="Open drawer" onClick={this.handleDrawerToggle} className={classes.menuButton}>
               <MenuIcon />
             </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
+            <Typography variant="h6" color="inherit" noWrap className={classes.typoTitle}>
               Design Strategy
             </Typography>
+            <Grid container justify="flex-end" alignItems="center">
+              <img alt="User 1" src={userOnline1} className={classes.bigAvatar} />
+              <img alt="User 2" src={userOnline2} className={classes.bigAvatar} />
+              <img alt="User 3" src={userOnline3} className={classes.bigAvatar} />
+              <Tooltip title="Setting">
+                <IconButton aria-label="Settings">
+                  <SettingsIcon style={{ fontSize: 40 }} className={classes.settings} />
+                </IconButton>
+              </Tooltip>
+            </Grid>
           </Toolbar>
         </AppBar>
         <nav className={classes.drawer}>
-          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-          <Hidden smUp implementation="css">
-            <Drawer
-              container={this.props.container}
-              variant="temporary"
-              anchor={theme.direction === "rtl" ? "right" : "left"}
-              open={this.state.mobileOpen}
-              onClose={this.handleDrawerToggle}
-              classes={{
-                paper: classes.drawerPaper
-              }}
-            >
-              {drawer}
-            </Drawer>
-          </Hidden>
-          <Hidden xsDown implementation="css">
-            <Drawer
-              classes={{
-                paper: classes.drawerPaper
-              }}
-              variant="permanent"
-              open
-            >
-              {drawer}
-            </Drawer>
-          </Hidden>
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
         </nav>
         <main className={classes.content}>
-          <Card className={classes.card}>
-            <CardHeader avatar={
-                <Avatar aria-label="Recipe" className={classes.avatar}>
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton>
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              subheader="September 14, 2016"
-            />
-            <CardMedia className={classes.media} image="/static/images/cards/paella.jpg" title="Paella dish"/>
-            <CardContent>
-              <Typography component="p">
-                This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.
-              </Typography>
-            </CardContent>
-            <CardActions className={classes.actions} disableActionSpacing>
-              <IconButton aria-label="Add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="Share">
-                <ShareIcon />
-              </IconButton>
-            </CardActions>
-          </Card>
-          <div className={classes.rootGridList}>
-            <GridList cellHeight={340} className={classes.gridList} cols={3}>
-              {tileData.map(tile => (
-                <GridListTile key={tile.img} cols={tile.cols || 1}>
-                  <img src={tile.img} alt={tile.title} />
-                </GridListTile>
-              ))}
-            </GridList>
+          <div className={classes.divParent}>
+            <div className={classes.divGestion}>
+              <Card className={classes.card}>
+                <CardHeader avatar={
+                    <Avatar aria-label="Recipe" className={classes.avatar}>
+                      R
+                    </Avatar>
+                  }
+                  action={
+                    <IconButton>
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  title="Shrimp and Chorizo Paella"
+                  subheader="September 14, 2016"
+                />
+                <CardMedia className={classes.media} image="/static/images/cards/paella.jpg" title="Paella dish"/>
+                <CardContent>
+                  <Typography component="p">
+                    This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                  </Typography>
+                </CardContent>
+                <CardActions className={classes.actions} disableActionSpacing>
+                  <IconButton aria-label="Add to favorites">
+                    <FavoriteIcon />
+                  </IconButton>
+                  <IconButton aria-label="Share">
+                    <ShareIcon />
+                  </IconButton>
+                </CardActions>
+              </Card>
+              <img className={classes.gestion} src={cardGestion} alt="card gestion" />
+              <img className={classes.doc} src={cardDoc} alt="card doc" />
+            </div>
+            <div className={classes.divVisio}>
+              <img className={classes.visio} src={cardVisio} alt="card visio" />
+            </div>
           </div>
         </main>
       </div>
--- src/components/Dashboard/styles.js
+++ src/components/Dashboard/styles.js
@@ -1,6 +1,7 @@
 import red from "@material-ui/core/colors/red";
 
 const drawerWidth = 240;
+const drawerColor = "#E2001A";
 
 export default theme => ({
   root: {
@@ -10,13 +11,25 @@ export default theme => ({
     [theme.breakpoints.up("sm")]: {
       width: drawerWidth,
       flexShrink: 0
-    }
+    },
   },
   appBar: {
     marginLeft: drawerWidth,
     [theme.breakpoints.up("sm")]: {
       width: `calc(100% - ${drawerWidth}px)`
-    }
+    },
+    backgroundColor: "#4A4A49"
+  },
+  typoTitle: {
+    width: "200px"
+  },
+  settings: {
+    marginLeft: "13px",
+    color: "#C6C6C6"
+  },
+  bigAvatar: {
+    margin: 3,
+    width: 40
   },
   menuButton: {
     marginRight: 20,
@@ -24,29 +37,19 @@ export default theme => ({
       display: "none"
     }
   },
-  toolbar: theme.mixins.toolbar,
   drawerPaper: {
-    width: drawerWidth
+    width: drawerWidth,
+    backgroundolor: drawerColor
   },
   content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-    marginTop: "50px"
-  },
-  rootGridList: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden",
-    backgroundColor: theme.palette.background.paper
-  },
-  gridList: {
-    width: 1300,
-    height: 900
+    marginTop: "75px",
+    display: "block"
   },
   card: {
     top: 50,
-    maxWidth: 400
+    maxWidth: 400,
+    paddingLeft: 2,
+    display: "inline-block"
   },
   media: {
     height: 0,
@@ -67,5 +70,34 @@ export default theme => ({
   },
   avatar: {
     backgroundColor: red[500]
+  },
+  divParent: {
+    display: "block"
+  },
+  divGestion: {
+    width: "880px",
+    height: "890px",
+    display: "inline-block",
+    paddingLeft: "20px"
+  },
+  gestion: {
+    height: "465px !important",
+    width: "420px",
+    padding: "0 !important",
+    display: "inline-block"
+  },
+  doc: {
+    width: "407px",
+    height: "420px",
+    padding: 0
+  },
+  divVisio: {
+    width: "300px",
+    height: "803px",
+    display: "inline-block"
+  },
+  visio: {
+    height: 473,
+    padding: 0
   }
 });
